e7cbac25551d47f180ed4ab46483ad5e2ed57f43
GGinthe <les95101@gmail.com>
2020-03-13 22:08:50 +0800

    Tutorial 1

---
+++ GitTutiruial-/GitTutiruial-.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include<string>
+#include<iostream>
+using namespace System;
+
+bool Guess(int number) {
+	return 0;
+}
+int main(array<System::String ^> ^args)
+{
+    return 0;
+}
