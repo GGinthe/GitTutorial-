508750a69909c52d474c6a336927577365f857d0
Saura0918 <sorashiro0603@gmail.com>
2020-03-14 23:29:39 +0800

    8B

--- GitTutiruial-/GitTutiruial-.cpp
+++ GitTutiruial-/GitTutiruial-.cpp
@@ -7,16 +7,20 @@ bool Guess(int number) {
 	static int target = -1;
 	if (target == -1) {
 		Random r;
-		target = r.Next() % 3 + 1;
+		target = r.Next() % 100 + 1;
 	}
-	if (number == target)return true;
-	return false;
+	if (number == target) {
+		std::cout << "Correct !!";
+		target = -1;
+		return true;
+	}
+	else std::cout << "Wrong" << std::endl;
+	return false;;
 }
 int main(array<System::String ^> ^args)
 {
 		int guess;
 		do {
-			std::cout << "Choose a number between 1 -100 :";
 			std::cin >> guess;
 		} while (!Guess(guess));
     return 0;
