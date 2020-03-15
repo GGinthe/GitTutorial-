38c9dd2d2cc787924cbac79e693639bbe8a4c2a7
GGinthe <les95101@gmail.com>
2020-03-15 17:06:35 +0800

    888

--- GitTutiruial-/GitTutiruial-.cpp
+++ GitTutiruial-/GitTutiruial-.cpp
@@ -6,12 +6,14 @@ using namespace System;
 bool Guess(int number) {
 	static int target = 0;
 	static int count = 0;
+	count++;
 	if (!target) {
 		Random r;
 		target = r.Next() % 100 + 1;
 	}
 	if (number == target) {
 		std::cout << "Correct !!";
+		std::cout << std::endl << count;
 		target = -1;
 		return true;
 	}else if(number > target)
